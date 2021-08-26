a2ac052d08f6eec96d228e3db91008f42ee1a862
arpitgoyal138 <arpitgoyal138@gmail.com>
2021-08-26 18:57:01 +0530

    SignIn and registeer added

---
+++ src/firebase/utils.js
@@ -0,0 +1,44 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { firebaseConfig } from "./config";
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
+
+const FacebookProvider = new firebase.auth.FacebookAuthProvider();
+FacebookProvider.addScope("user_birthday");
+export const signInWithFacebook = () => {
+  auth.signInWithPopup(FacebookProvider);
+};
+
+export const handleUserProfile = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const { uid, displayName, email } = userAuth;
+  const userRef = firestore.doc(`users/${uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const timestamp = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt: timestamp,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return userRef;
+};
+
+// export const signInWithEmailPassword = () => {
+//   auth.
+// }
